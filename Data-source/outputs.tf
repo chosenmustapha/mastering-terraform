9a0770d0a8ee35ee5029517b9c4f8946b954ba12
Mustapha Barrie <mbarrie07@gmail.com>
2026-05-24 18:37:03 -0400

    update output.tf

--- Data-source/outputs.tf
+++ Data-source/outputs.tf
@@ -17,6 +17,21 @@ output "aws_ami_architecture" {
   value = data.aws_ami.ubuntu_26_04_arm64.architecture
 }
 
+# Debug:
+# output "subnet_map_public_ip_on_launch" {
+#   value       = data.aws_subnet.dev_subnets.map_public_ip_on_launch
+#   description = "true if instances launched in this subnet receive a public IP by default"
+# }
+
+output "subnet_public_or_private" {
+  value       = data.aws_subnet.dev_subnets.map_public_ip_on_launch ? "public" : "private"
+  description = "print'public' if map_public_ip_on_launch is true, otherwise 'private'"
+}
+
+output "subnet_az" {
+  value = data.aws_subnet.dev_subnets.availability_zone
+}
+
 output "aws_instance_tag" {
   value = { for k, inst in aws_instance.my-ubuntu-vm : k => lookup(inst.tags, "Name", "") }
   description = "Map of instance key to the instance Name tag"
