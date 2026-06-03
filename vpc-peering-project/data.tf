ddc9a9ae2509b772696f9918f123ec595d879578
Mustapha Barrie <mbarrie07@gmail.com>
2026-06-03 17:17:50 -0400

    Implement data.tf for the two ec2 instances

---
+++ vpc-peering-project/data.tf
@@ -0,0 +1,49 @@
+data "aws_ami" "east_instance" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["*26.04*", "*26.04 LTS*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "west_instance" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["*26.04*", "*26.04 LTS*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
