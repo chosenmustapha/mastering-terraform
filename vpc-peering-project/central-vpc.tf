5f3d15c29877cab3221c4e4f54135d072698208f
Mustapha Barrie <mbarrie07@gmail.com>
2026-06-07 15:35:16 -0400

    fix route cidr_block mistake

--- vpc-peering-project/central-vpc.tf
+++ vpc-peering-project/central-vpc.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "central_route_table" {
   vpc_id   = aws_vpc.central.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.central_igw.id
   }
 }
