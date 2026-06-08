a660ad8eebf5287ccc77b1c2816a468e659d841e
Mustapha Barrie <mbarrie07@gmail.com>
2026-06-08 16:50:02 -0400

    Implement peering connection between East -> central

--- vpc-peering-project/peering.tf
+++ vpc-peering-project/peering.tf
@@ -1,4 +1,5 @@
 
+# East-West VPC Peering Connection
 resource "aws_vpc_peering_connection" "east_west_peering" {
   provider      = aws.East_region
   peer_owner_id = data.aws_caller_identity.current.account_id
@@ -21,6 +22,32 @@ resource "aws_vpc_peering_connection_accepter" "east_west_peering_accepter" {
 
   tags = {
     Name = "east-west-peering-accepter"
+  }
+}
+
+# East-Central VPC Peering Connection
+resource "aws_vpc_peering_connection" "east_central_peering" {
+  provider     = aws.East_region
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  vpc_id        = aws_vpc.east.id
+  peer_vpc_id   = aws_vpc.central.id
+  auto_accept   = false
+  peer_region   = var.canada_region
+
+  tags = {
+    Name = "east-central-peering"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "east_central_peering_accepter" {
+  provider                  = aws.Canada_region
+  vpc_peering_connection_id = aws_vpc_peering_connection.east_central_peering.id
+  auto_accept               = true
+
+  depends_on = [aws_vpc_peering_connection.east_central_peering]
+
+  tags = {
+    Name = "east-central-peering-accepter"
   }
 }
 
