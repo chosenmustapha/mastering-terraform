cce19896de1a44d602c4ac08bd09511a6203a63c
Mustapha Barrie <mbarrie07@gmail.com>
2026-06-09 16:47:16 -0400

    Create ami data look up for central region

--- vpc-peering-project/data.tf
+++ vpc-peering-project/data.tf
@@ -1,3 +1,6 @@
+
+# east region AMI data source
+
 data "aws_ami" "east_instance" {
   provider    = aws.East_region
   most_recent = true
@@ -24,6 +27,7 @@ data "aws_ami" "east_instance" {
   }
 }
 
+# West region AMI data source
 data "aws_ami" "west_instance" {
   provider    = aws.West_region
   most_recent = true
@@ -49,5 +53,35 @@ data "aws_ami" "west_instance" {
     values = ["ebs"]
   }
 }
+
+
+# Central region AMI data source
+data "aws_ami" "central_instance" {
+  provider    = aws.Canada_region
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["*26.04*", "*26.04 LTS*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+
+
 
 data "aws_caller_identity" "current" {}
