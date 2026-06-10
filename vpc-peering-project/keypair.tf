55231eb312e2ebb4348c25aa86bc171abbe35ed8
Mustapha Barrie <mbarrie07@gmail.com>
2026-06-10 16:35:18 -0400

    implement central region ssh key-pair

--- vpc-peering-project/keypair.tf
+++ vpc-peering-project/keypair.tf
@@ -1,11 +1,20 @@
+# est region key pair
 resource "aws_key_pair" "east_key_pair" {
   provider   = aws.East_region
   key_name   = "east-key-pair"
   public_key = file("~/.ssh/my-aws-key.pub")
 }
 
+# west region key pair
 resource "aws_key_pair" "west_key_pair" {
   provider   = aws.West_region
   key_name   = "west-key-pair"
+  public_key = file("~/.ssh/my-aws-key.pub")
+}
+
+# Central region key pair
+resource "aws_key_pair" "central_key_pair" {
+  provider = aws.Canada_region
+  key_name = "central-key-pair"
   public_key = file("~/.ssh/my-aws-key.pub")
 }
