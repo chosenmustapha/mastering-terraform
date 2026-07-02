15891846aa0fafae34e950d239f17b8eb6af2576
Mustapha Barrie <mbarrie07@gmail.com>
2026-07-02 15:34:13 -0400

    Implement application load balancer, listener and traffic stwitch

--- blue-green-deployment/main.tf
+++ blue-green-deployment/main.tf
@@ -151,9 +151,32 @@ resource "aws_lb_target_group" "green" {
     interval            = 30
     matcher             = "200"
   }
-  
+
   tags = {
     name = "green-target-group"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "blue-green-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    name = "blue-green-alb"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.active_environment == "blue" ? aws_lb_target_group.blue.arn : aws_lb_target_group.green.arn
   }
 }
 
