4612eeb7d531df96d8dff7338b8907da669d911a
Mustapha Barrie <mbarrie07@gmail.com>
2026-07-14 20:48:06 -0400

    Implement iam roles and policy

---
+++ serverless-file-audit-pipeline/iam.tf
@@ -0,0 +1,53 @@
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "services"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "${var.project_name}-lambda-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+data "aws_iam_policy_document" "lambda_permissions" {
+
+  statement {
+    sid       = "AllowS3Read"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.uploads.arn}/*"]
+  }
+
+  statement {
+    sid       = "AllowDynamoDBWrite"
+    actions   = ["dynamodb:PutItem"]
+    resources = [aws_dynamodb_table.audit_log.arn]
+  }
+
+  statement {
+    sid       = "AllowSNSPublish"
+    actions   = ["sns:Publish"]
+    resources = [aws_sns_topic.upload_notifications.arn]
+  }
+
+  statement {
+    sid = "AllowCloudWatchLogs"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:*"]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_permissions" {
+  name   = "${var.project_name}-lambda-permissions"
+  role   = aws_iam_role.lambda_exec_role.id
+  policy = data.aws_iam_policy_document.lambda_permissions.json
+}
