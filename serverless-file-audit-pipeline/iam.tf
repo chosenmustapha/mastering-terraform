3c2048b41f3a382c5cba1571d5141c278791e8a2
Mustapha Barrie <mbarrie07@gmail.com>
2026-07-18 22:24:31 -0400

    Fixed IAM Policy principal typo from 'Services' to Service'

--- serverless-file-audit-pipeline/iam.tf
+++ serverless-file-audit-pipeline/iam.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "lambda_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      type        = "Services"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
