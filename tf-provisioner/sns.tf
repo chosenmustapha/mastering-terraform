6467af5e9c9922d55a7c7faacb984da08a102b5f
Mustapha Barrie <mbarrie07@gmail.com>
2026-07-31 10:25:10 -0400

    renamed deploy-notifications to deploy_notifications to stay consistent

--- tf-provisioner/sns.tf
+++ tf-provisioner/sns.tf
@@ -1,10 +1,10 @@
 
-resource "aws_sns_topic" "deploy-notifications" {
+resource "aws_sns_topic" "deploy_notifications" {
   name = "${var.project_name}-deploy-notifications"
 }
 
 resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.deploy-notifications.arn
+  topic_arn = aws_sns_topic.deploy_notifications.arn
   protocol  = "email"
   endpoint  = var.notification_email
 }
