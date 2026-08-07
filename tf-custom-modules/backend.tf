22ebf18f7ef0c82e32fc33b8b5c79cf9ce11e409
Mustapha Barrie <mbarrie07@gmail.com>
2026-08-07 09:30:57 -0400

    chore: created remote backend and provider for this project, no implementation yet

---
+++ tf-custom-modules/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "amazon-remote-s3-backend"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
