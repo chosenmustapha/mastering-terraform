672eba30c077d1391a272936f11ffa3a0178f978
Mustapha Barrie <mbarrie07@gmail.com>
2026-08-09 17:09:43 -0400

    Implement the root module variables file

--- modular-web-infra/variables.tf
+++ modular-web-infra/variables.tf
@@ -1,5 +1,44 @@
-variable "region" {
-  description = "The AWS region to deploy resources in."
-  type        = string
-  default     = "us-east-1"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "modular-web-demo"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "asg_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "asg_max_size" {
+  type    = number
+  default = 4
+}
+
+variable "ssh_allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0" 
 }
