53a866fcfd4c9da48ff92dbd6abf4d1117518ace
Mustapha Barrie <mbarrie07@gmail.com>
2026-08-19 21:12:52 -0400

    Implement variable file for compute module

---
+++ modular-web-infra/modules/compute/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name"      { type = string }
+variable "instance_type"     { type = string }
+variable "vpc_id"            { type = string }
+variable "public_subnet_ids" { type = list(string) }
+variable "alb_sg_id"         { type = string }
+variable "web_sg_id"         { type = string }
+variable "bucket_name"       { type = string }
+variable "bucket_arn"        { type = string }
+variable "asg_min_size"      { type = number }
+variable "asg_max_size"      { type = number }
