c6cd0a788a07c371a1362ed0c712aa3e8eaf0113
Mustapha Barrie <mbarrie07@gmail.com>
2026-08-23 19:51:30 -0400

    reference of security on the root module

--- modular-web-infra/main.tf
+++ modular-web-infra/main.tf
@@ -6,3 +6,11 @@ module "networking" {
   public_subnet_cidrs = var.public_subnet_cidrs
   azs                 = var.azs
 }
+
+module "security" {
+  source = "./modules/security"
+
+  project_name     = var.project_name
+  vpc_id           = module.networking.vpc_id
+  ssh_allowed_cidr = var.ssh_allowed_cidr
+}
