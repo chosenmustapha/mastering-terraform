b6fcef5418b7933d66802c59f2fe749feecc3d6c
Mustapha Barrie <mbarrie07@gmail.com>
2026-08-25 21:48:09 -0400

    reference compute  on the root module

--- modular-web-infra/main.tf
+++ modular-web-infra/main.tf
@@ -19,3 +19,18 @@ module "storage" {
 
   project_name = var.project_name
 }
+
+module "compute" {
+  source = "./modules/compute"
+
+  project_name      = var.project_name
+  instance_type     = var.instance_type
+  vpc_id            = module.networking.vpc_id
+  public_subnet_ids = module.networking.public_subnet_ids
+  alb_sg_id         = module.security.alb_sg_id
+  web_sg_id         = module.security.web_sg_id
+  bucket_name       = module.storage.bucket_name
+  bucket_arn        = module.storage.bucket_arn
+  asg_min_size      = var.asg_min_size
+  asg_max_size      = var.asg_max_size
+}
